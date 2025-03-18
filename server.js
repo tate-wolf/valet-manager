993caaf7d7a99dad72a5f949cda71e4ab02615cd
tate-wolf <tate.wolfgramm@gmail.com>
2025-03-18 08:31:56 -0400

    Switch from bcrypt to bcryptjs to test deployment compatibility

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
 const path = require('path');
 const db = require('./db');
